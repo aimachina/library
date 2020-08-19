ec84fbba7090b13e638c43c4708ff640bc1c25f9
fadmaro-code <53923179+fadmaro-code@users.noreply.github.com>
2020-08-19 14:52:30 -0500

    Behind nginx (#19)
    * Add proxyxfix settings
    
    * Fix parenthesis error

--- app_factories/app_factory.py
+++ app_factories/app_factory.py
@@ -11,6 +11,8 @@ from app_factories.api_factory import create_api
 from utils.configmanager import ConfigManager
 from utils.common import make_url
 
+# Enable for serving over https
+from werkzeug.contrib.fixers import ProxyFix
 
 logging.getLogger("flask_cors").level = logging.DEBUG
 
@@ -19,6 +21,8 @@ def make_flask():
     flask_app = Flask(__name__)
     flask_app.config["SECRET_KEY"] = str(uuid.uuid4())
     flask_app.config["MAX_CONTENT_LENGTH"] = 8 * 1024 * 1024
+    flask_app.url_map.strict_slashes = False
+    flask_app.wsgi_app = ProxyFix(flask_app.wsgi_app)
 
     if "database" in ConfigManager.get_config():
         config_database(flask_app, ConfigManager.get_config_value("database"))
