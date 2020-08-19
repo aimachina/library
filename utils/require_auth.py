163d0a79337bd1b57b26a6411052a296873b8a40
fadmaro <fabian.duque@code-ing.com>
2020-08-19 16:17:41 -0500

    Add apikey validation in require_auth

--- utils/require_auth.py
+++ utils/require_auth.py
@@ -4,9 +4,11 @@ from flask import Response
 from utils.configmanager import ConfigManager
 
 conf = ConfigManager.get_config_value('ory')
+apis = ConfigManager.get_config_value('apis')
 hydra_config = conf['oauth2']['hydra']
 oauth2_client = conf['oauth2']['client']
 
+api_key = apis['tiketai']['apikey']
 
 HYDRA_HOST = hydra_config['host']
 HYDRA_PUBLIC_PORT = hydra_config['public_port']
@@ -31,9 +33,9 @@ def __token_introspection(access_token):
         response = post(f'{HYDRA_HOST}:{HYDRA_ADMIN_PORT}/oauth2/introspect', data=data, verify=False)
         return response.status_code, response.json()
 
-def require_auth(request):
+def require_auth(request, auth_type='oauth2'):
     def _wrapper(fn):
-        def _inner_wrapper(*args, **kwargs):
+        def _validate_oauth2(*args, **kwargs):
             authorization = request.headers.get('Authorization')
             if authorization == None:
                 return Response(None, status=403)
@@ -55,7 +57,20 @@ def require_auth(request):
                 'sub': data['sub'],
                 'claims': data['ext']['claims']
             }
-
             return fn(*args, claims=claims, **kwargs)
-        return _inner_wrapper
+
+        def _validate_apikey(*args, **kwargs):
+            if auth_type == 'apikey':
+                apikey = request.headers.get('X-API-KEY')
+                if apikey == None:
+                    return Response(None, status=403)
+
+                if apikey != api_key
+                    return Response(None, status=403)
+                return fn(*args, **kwargs)
+
+        if auth_type == 'oauth2'
+            return _validate_oauth2
+        else:
+            return _validate_apikey
     return _wrapper
