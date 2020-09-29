63c4303255f58d94c0694373939f4984140701ec
fadmaro <fabian.duque@code-ing.com>
2020-09-28 19:20:42 -0500

    Remove utils.dicts

--- utils/dicts.py
+++
@@ -1,6 +0,0 @@
-from inspect import getmembers, isroutine
-
-def class_to_dict(obj):
-    attrs = getmembers(obj), lambda a:not(isroutine(a))
-    attrs = [a for a in attrs[0] if not(a[0].startswith('__') and a[0].endswith('__'))]
-    return dict(attrs)
