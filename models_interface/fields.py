686cb5570b2ef04e21f5d39d03feef3569e82b44
NestorMejiaCode <72581834+NestorMejiaCode@users.noreply.github.com>
2021-05-03 19:19:27 -0500

    common fields (#61)
    Co-authored-by: Nestor <programatonpro@gmail.com>

---
+++ models_interface/fields.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class StringFieldDetails(BaseModel):
+    value: str = None
+    uuid_line: str = None
+    field_label: Optional[str] 
+
+
+class DateTimedDetails(BaseModel):
+    value: datetime = None
+    uuid_line: str = None
