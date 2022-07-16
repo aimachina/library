250959e3a3f22b8bbbb02842f3bc3e9cc394909b
tequilaguru <40527414+tequilaguru@users.noreply.github.com>
2022-07-15 20:17:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aim-library",
-    version="0.0.16",
+    version="0.0.17",
     author="AIMachina",
     author_email="ticketai@outlook.com",
     description="AIMachina library",
