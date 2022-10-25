40328b3dd0810c70a787ef6d2193836a438c8246
fadmaro <fabian.duque@code-ing.com>
2022-10-25 13:15:49 -0500

    Add warning

--- aim_library/utils/singleton.py
+++ aim_library/utils/singleton.py
@@ -11,4 +11,5 @@ class Singleton(type):
         instance  = cls._instances[cls]
         for k,v in kwargs.items():
             setattr(instance, k, v)
+        print(f'WARNING! {cls.__name__} is a Singleton')
         return instance
