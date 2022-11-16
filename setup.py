96b179487c5aa8ca8b24cd119f83659cbc9bb96a
Chepe Ruiz <chepe.ruiz@gmail.com>
2022-11-16 15:36:04 -0800

    Bumped release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aim-library",
-    version="0.1.0"
+    version="0.1.0",
     author="AIMachina",
     author_email="ticketai@outlook.com",
     description="AIMachina library",
